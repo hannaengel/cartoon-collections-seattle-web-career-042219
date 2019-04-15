2916619ca5f0941af2eba5967e265ce15f6c401c
Hanna Engel <hengel@alumni.berklee.edu>
2019-04-15 03:56:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,8 +18,9 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(strings)
-  cheese_types = ["cheddar", "gouda", "camembert"]	 
+  cheese_types = ["cheddar", "gouda", "camembert"]
   strings.find do |x|
     cheese_types.include?(x)
+    #find returns first instance of true or nil if false
   end
 end
